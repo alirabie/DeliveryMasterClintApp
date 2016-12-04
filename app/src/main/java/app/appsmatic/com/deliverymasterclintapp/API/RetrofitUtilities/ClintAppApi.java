e7eacd04d47c1511bbaa898328b648a5f53fd721
Ali <ai.rabie89@hotmail.com>
2016-12-04 14:14:40 +0200

    Api Config

---
+++ app/src/main/java/app/appsmatic/com/deliverymasterclintapp/API/RetrofitUtilities/ClintAppApi.java
@@ -0,0 +1,7 @@
+package app.appsmatic.com.deliverymasterclintapp.API.RetrofitUtilities;
+
+/**
+ * Created by Mido PC on 12/4/2016.
+ */
+public interface ClintAppApi {
+}
---
+++ app/src/main/java/app/appsmatic/com/deliverymasterclintapp/API/RetrofitUtilities/Genrator.java
@@ -0,0 +1,24 @@
+package app.appsmatic.com.deliverymasterclintapp.API.RetrofitUtilities;
+
+import okhttp3.OkHttpClient;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+/**
+ * Created by Mido PC on 12/4/2016.
+ */
+public class Genrator {
+    private static final String API_URL = "http://www.5osomaat.com/WebService/API.ASMX/";
+
+    private static OkHttpClient.Builder httpClient = new OkHttpClient.Builder();
+
+    private static Retrofit.Builder builder =
+            new Retrofit.Builder()
+                    .baseUrl(API_URL)
+                    .addConverterFactory(GsonConverterFactory.create());
+
+    public static <S> S createService(Class<S> serviceClass) {
+        Retrofit retrofit = builder.client(httpClient.build()).build();
+        return retrofit.create(serviceClass);
+    }
+}
